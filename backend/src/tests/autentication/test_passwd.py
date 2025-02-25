deac431576cccef315eef382db065608ae95006b
AleksRusa <rusakovmen@gmail.com>
2025-02-25 18:16:08 +0300

    wrote tests for password

---
+++ backend/src/tests/autentication/test_passwd.py
@@ -0,0 +1,23 @@
+import pytest
+
+from backend.src.auth.utils import hash_password, validate_password
+
+@pytest.mark.parametrize("password", ["password", "1234346432", "PIk21][;!]"])
+def test_hash_password(password):
+    hashed_password = hash_password(password)
+
+    assert isinstance(hashed_password, bytes), "Хеш пароля должен быть типа bytes"
+    assert len(hashed_password) > 0, "Хеш пароля не должен быть пустым"
+
+@pytest.mark.parametrize("password", ["password", "1234346432", "PIk21][;!]"])
+def test_verify_password(password):
+    hashed_password = hash_password(password)
+
+    assert validate_password(password, hashed_password)
+
+@pytest.mark.parametrize("password", ["password", "1234346432", "PIk21][;!]"])
+def test_verify_wrong_password(password):
+    wrong_password = "wrongpas"
+    hashed_password = hash_password(password)
+
+    assert not validate_password(wrong_password, hashed_password)
