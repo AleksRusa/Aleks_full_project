a210a5e68e6a4bca7e205c8bf5ec9199d3c8735e
AleksRusa <rusakovmen@gmail.com>
2025-04-01 14:32:18 +0300

    now redis_url is getting from environment

--- backend/src/redis_client.py
+++ backend/src/redis_client.py
@@ -1,8 +1,14 @@
+import os
+
 import redis.asyncio as redis
 
+from config.config import settings
+
+REDIS_URL_async = settings.redis_url_aioredis
+redis_url = os.environ.get('REDIS_URL') or REDIS_URL_async
 
 async def get_redis():
-    redis_client = await redis.from_url("redis://localhost:6379")
+    redis_client = await redis.from_url(url=redis_url)
     try:
         yield redis_client
     finally:
