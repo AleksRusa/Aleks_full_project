4e534b75b04c48b48ecc4b57768d98019155954e
AleksRusa <rusakovmen@gmail.com>
2025-04-01 15:40:43 +0300

    saving before going to main

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
   ],
   base: "/",
   server: {
-    port: 80, // Указываем порт 80
+    host: "0.0.0.0",
+    port: 3000, // Указываем порт 80
     strictPort: true, // Если порт занят, приложение не запустится
+    watch: {
+      usePolling: true
+    }
   }
 
 });
